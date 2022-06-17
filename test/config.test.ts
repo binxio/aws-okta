089461bfe214283557ea910558ee7b48a86050b3
Daniel Molenaars <danielmolenaars@binx.io>
2022-06-17 13:20:56 +0200

    test: add mock html

--- test/config.test.ts
+++ test/config.test.ts
@@ -2,23 +2,23 @@ import { getEnvVariables, getSanitizedEnvVars } from "../src/config"
 
 describe("test environment configuration", () => {
     test('it should return the set value', () => {
-    const variables = getEnvVariables("test/env")
-    const sanitizedVariables = getSanitizedEnvVars(variables)
+        const variables = getEnvVariables("test/mock-files/env")
+        const sanitizedVariables = getSanitizedEnvVars(variables)
         expect(sanitizedVariables.OKTA_AWS).toBe("https://banana.icecream");
     });
 
 
     test('it should throw an error when a required key is missing', () => {
-    const variables = getEnvVariables("test/env-missing-key")
-    const sanitizedVariables = () => {
-        getSanitizedEnvVars(variables);
-    };
+        const variables = getEnvVariables("test/mock-files/env-missing-key")
+        const sanitizedVariables = () => {
+            getSanitizedEnvVars(variables);
+        };
         expect(sanitizedVariables).toThrow("Missing key OKTA_AWS in env");
     });
 
 
     test('it should throw an error if a required key has no value', () => {
-        const variables = getEnvVariables("test/env-missing-value")
+        const variables = getEnvVariables("test/mock-files/env-missing-value")
         const sanitizedVariables = () => {
             getSanitizedEnvVars(variables);
         };
